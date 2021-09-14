d19a09cafe727be48dd0b2ed454273f776bd0f9b
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 23:23:51 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print("Please Enter your first number ")
+nmbr1= input()
+print("Please Enter your second number ")
+nmbr2= input()
+print("The sum of these two numbers is", int(nmbr1)+int(nmbr2))
+
+
+
+
+
+
